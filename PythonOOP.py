6f0908701dcf44b283b035673be5a662e041db0d
Jakub Samul <jakubsamul0@gmail.com>
2022-11-23 11:24:43 +0100

    'python-oop-tutorial3-classmethod-and-staticmethod'

--- PythonOOP.py
+++ PythonOOP.py
@@ -1,7 +1,7 @@
 class Employee:
 
     num_of_emps = 0
-    raise_amount = 1.04
+    raise_amt = 1.04
 
     def __init__(self, first, last, pay):
         self.first = first
@@ -15,12 +15,36 @@ class Employee:
         return '{} {}'.format(self.first, self.last)
 
     def apply_raise(self):
-        self.pay =int(self.pay * self.raise_amount)
+        self.pay =int(self.pay * self.raise_amt)
 
-print(Employee.num_of_emps)
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
 
 emp_1 = Employee('Corey', 'CCCCC', 50000)
 emp_2 = Employee('JOHN', 'AAAAA', 60000)
 
-print(Employee.num_of_emps)
+# emp_str_1 = 'John-Doe-70000'
+# emp_str_2 = 'Steve-Smith-30000'
+# emp_str_3 = 'Jane-Doe-90000'
 
+# new_emp_1 = Employee.from_string(emp_str_1)
+
+# print(new_emp_1.email)
+# print(new_emp_1.pay)
+
+import datetime
+my_date = datetime.date(2016, 7, 11)
+
+print(Employee.is_workday(my_date))
