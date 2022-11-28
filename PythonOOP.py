24480ecee269afaf0562bf60b073fe8bf3834723
Jakub Samul <jakubsamul0@gmail.com>
2022-11-28 14:09:04 +0100

    'property-decorators-getters-setters-deleters'

--- PythonOOP.py
+++ PythonOOP.py
@@ -1,47 +1,38 @@
 class Employee:
 
-    num_of_emps = 0
-    raise_amt = 1.04
-
-    def __init__(self, first, last, pay):
+    def __init__(self, first, last):
         self.first = first
         self.last = last
-        self.pay = pay
-        self.email = first + '-' + last + '@company.com'
 
+    @property
+    def email(self):
+        return '{}.{}@email.com'.format(self.first, self.last)
+
+    @property
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
-    def apply_raise(self):
-        self.pay =int(self.pay * self.raise_amt)
+    @fullname.setter
+    def fullname(self, name):
+        first, last = name.split(' ')
+        self.first = first
+        self.last = last
 
-    def apply_raise(self):
-        self.pay = int(self.pay + self.raise_amt)
-    
-    def __repr__(self):
-        return "Employee('{}', '{}', '{}')".format(self.first, self.last, self.pay)
+    @fullname.deleter
+    def fullname(self):
+        print('Delete Name!')
+        self.first = None
+        self.last = None
+        
 
-    def __str__(self):
-        return '{} - {}'.format(self.fullname(), self.email)
 
-    def __add__(self, other):
-        return self.pay + other.pay
+emp_1 = Employee('Corey', 'CCCCC')
 
-    def __len__(self):
-        return len(self.fullname())
+emp_1.fullname = "Jakub Samul"
 
-emp_1 = Employee('Corey', 'CCCCC', 50000)
-emp_2 = Employee('JOHN', 'AAAAA', 60000)
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.fullname)
 
-print(len(emp_1))
-
-# print(emp_1 + emp_2)
-
-# print(emp_1)
-
-# print(repr(emp_1))
-# print(str(emp_1))
-
-# print(emp_1.__repr__())
-# print(emp_1.__str__())
+del emp_1.fullname
 
