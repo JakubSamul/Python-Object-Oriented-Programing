eaa58c4b66ff391713506701b61f451dcdd15bfa
Jakub Samul <jakubsamul0@gmail.com>
2022-11-28 13:44:07 +0100

    'inheritance-creating-subclasses'

--- PythonOOP.py
+++ PythonOOP.py
@@ -9,42 +9,39 @@ class Employee:
         self.pay = pay
         self.email = first + '-' + last + '@company.com'
 
-        Employee.num_of_emps += 1
-
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
     def apply_raise(self):
         self.pay =int(self.pay * self.raise_amt)
 
-    @classmethod
-    def set_raise_amt(cls, amount):
-        cls.raise_amt = amount
+    def apply_raise(self):
+        self.pay = int(self.pay + self.raise_amt)
+    
+    def __repr__(self):
+        return "Employee('{}', '{}', '{}')".format(self.first, self.last, self.pay)
 
-    @classmethod
-    def from_string(cls, emp_str):
-        first, last, pay = emp_str.split('-')
-        return cls(first, last, pay)
+    def __str__(self):
+        return '{} - {}'.format(self.fullname(), self.email)
 
-    @staticmethod
-    def is_workday(day):
-        if day.weekday() == 5 or day.weekday() == 6:
-            return False
-        return True
+    def __add__(self, other):
+        return self.pay + other.pay
+
+    def __len__(self):
+        return len(self.fullname())
 
 emp_1 = Employee('Corey', 'CCCCC', 50000)
 emp_2 = Employee('JOHN', 'AAAAA', 60000)
 
-# emp_str_1 = 'John-Doe-70000'
-# emp_str_2 = 'Steve-Smith-30000'
-# emp_str_3 = 'Jane-Doe-90000'
+print(len(emp_1))
 
-# new_emp_1 = Employee.from_string(emp_str_1)
+# print(emp_1 + emp_2)
 
-# print(new_emp_1.email)
-# print(new_emp_1.pay)
+# print(emp_1)
 
-import datetime
-my_date = datetime.date(2016, 7, 11)
+# print(repr(emp_1))
+# print(str(emp_1))
 
-print(Employee.is_workday(my_date))
+# print(emp_1.__repr__())
+# print(emp_1.__str__())
+
